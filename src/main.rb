9f52c215ae567bebdc5107d5f6afd787c4cec8c7
mkkekkonen <mkkekkonen@protonmail.com>
2019-12-23 08:04:50 +0200

    Guess functionality

--- src/main.rb
+++ src/main.rb
@@ -5,4 +5,16 @@ require_relative 'rnd/rnd'
 rnd = Rnd.new
 rnd.create_random_number
 
-puts rnd.random_number
+guess = nil
+
+while guess != rnd.random_number do
+  puts 'Guess a number:'
+  guess = gets.chomp.to_i
+  if guess > rnd.random_number
+    puts 'Too large'
+  elsif guess < rnd.random_number
+    puts 'Too small'
+  end
+end
+
+puts 'Correct, congrats!'
